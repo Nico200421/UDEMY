80e161d1a6b177436b98ecf66ba3cf1cfbc4da93
Nico200421 <129789285+Nico200421@users.noreply.github.com>
2023-07-01 20:52:19 -0500

    Add files via upload

---
+++ Practica1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Este programa ejemplifica el uso del entorno de desarrollo para escribir
+programas en lenguaje C, mediante una instruccion que muestra mensaje de bienvenida por la consola*/
+int main()
+{
+    //Muestra por pantalla el mensaje indicado
+    printf("hola mundo lenguaje C\n");
+    return 0;
+}
